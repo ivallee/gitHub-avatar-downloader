31981983f2ed33ceada3f07f84cceb81b5102a92
Isaac Vallee <isaacvallee@gmail.com>
2017-09-12 02:57:43 +0000

    linted

--- download-avatars.js
+++ download-avatars.js
@@ -11,30 +11,30 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 // Accesses repo contributors
 function getRepoContributors(repoOwner, repoName, cb) {
-  const requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  const requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   const options = {
-      url: requestURL,
-      headers: {
-        'User-Agent': 'Student project - github avatar downloader'
-      }
+    url: requestURL,
+    headers: {
+      'User-Agent': 'Student project - github avatar downloader'
+    }
   };
 
   request(options, function (err, response, body){
     const content = JSON.parse(body);
     cb(err, content);
-  })
+  });
 }
 
 // Downloads URL and writes to specified path
 function downloadImageByURL(url, filePath) {
   request.get(url)
-  .on('error', function (err) {
-    throw err;
-  })
-  .on('response', function (response) {
-    console.log('Downloaded image to: ' + filePath);
-  })
-  .pipe(fs.createWriteStream(filePath));
+    .on('error', function (err) {
+      throw err;
+    })
+    .on('response', function (response) {
+      console.log('Downloaded image to: ' + filePath);
+    })
+    .pipe(fs.createWriteStream(filePath));
 }
 
 
@@ -42,12 +42,12 @@ getRepoContributors(arg1, arg2, function(err, result) {
   console.log("Errors:", err);
   // Checks for arguments from command line
   if (!arg1 || !arg2) {
-    console.log("Please enter a valid repo owner and rep name")
+    console.log("Please enter a valid repo owner and rep name");
   } else {
     for (var i = 0; i < result.length; i++) {
-        const url = result[i].avatar_url;
-        const name = result[i].login;
-        downloadImageByURL(url, './avatars/' + name + '.jpg');
-      }
+      const url = result[i].avatar_url;
+      const name = result[i].login;
+      downloadImageByURL(url, './avatars/' + name + '.jpg');
+    }
   }
 });
