d85ff057151ba978122d473df485a363f6d475ab
Isaac Vallee <isaacvallee@gmail.com>
2017-09-12 02:39:04 +0000

    tied functions together

--- download-avatars.js
+++ download-avatars.js
@@ -6,42 +6,42 @@ const GITHUB_TOKEN = "1dd1219e875ed6dd517c857ebd87bbc26eb169f8";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// Accesses repo contributors
 function getRepoContributors(repoOwner, repoName, cb) {
   const requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   const options = {
       url: requestURL,
       headers: {
         'User-Agent': 'Student project - github avatar downloader'
-    }
-};
+      }
+  };
 
   request(options, function (err, response, body){
     const content = JSON.parse(body);
-    // Prints avatar URLS
-    for (var i = 0; i < content.length; i++) {
-      console.log(content[i].avatar_url);
-    }
+    cb(err, content);
   })
-
-
 }
 
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//   console.log("Errors:", err);
-//   console.log("Result:", result);
-// });
-
+// Downloads URL and writes to specified path
 function downloadImageByURL(url, filePath) {
   request.get(url)
   .on('error', function (err) {
     throw err;
   })
   .on('response', function (response) {
-    console.log('Response Message: ', response.statusMessage + "\n", response.headers["content-type"]);
-    console.log('Download Complete');
+    console.log('Downloaded image to: ' + filePath);
   })
   .pipe(fs.createWriteStream(filePath));
 }
 
 
-downloadImageByURL('https://avatars0.githubusercontent.com/u/1615?v=4', './avatars/image.jpg');
+getRepoContributors("jquery", "jquery", function(err, result) {
+  // Loops through contributor array and calls image downloader
+  console.log("Errors:", err);
+  console.log("Result:", result);
+  for (var i = 0; i < result.length; i++) {
+      const url = result[i].avatar_url;
+      const name = result[i].login;
+      downloadImageByURL(url, './avatars/' + name + '.jpg');
+    }
+});
