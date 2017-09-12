40e1a274d2adf17bf38c54e69fbd43f779ef27d2
Isaac Vallee <isaacvallee@gmail.com>
2017-09-12 01:40:32 +0000

    logg avatar urls

--- download-avatars.js
+++ download-avatars.js
@@ -7,26 +7,24 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   const requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
   const options = {
-    url: requestURL,
-    headers: {
-      'User-Agent': 'Student project - github avatar downloader'
-  }
+      url: requestURL,
+      headers: {
+        'User-Agent': 'Student project - github avatar downloader'
+    }
 };
-  request.get(options)
-    .on('error', function (err) {
-      throw err;
-    })
-    .on('response', function (response) {
-      response.on('data', function (chunk) {
-        console.log('BODY: ' + chunk);
-      })
-          })
+
+  request(options, function (err, response, body){
+    const content = JSON.parse(body);
+    for (var i = 0; i < content.length; i++) {
+      console.log(content[i].avatar_url);
+    }
+  })
+
 
 }
 
- getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
